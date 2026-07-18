4d6dae77945b39890f63756463633a696b490878
Nandini Mukherjee <158547260+Nandini-556@users.noreply.github.com>
2026-07-18 13:27:24 +0530

    Time: 23 ms (11.8%), Space: 12.3 MB (89.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        original_number = x
+        reversed_number = 0
+
+        while x > 0:
+            digit = x % 10
+            reversed_number = reversed_number * 10 + digit
+            x //= 10
+
+        return original_number == reversed_number
