0c714e308aca6ae30da2ee4dcb09c566723ed2e8
Nandini Mukherjee <158547260+NandiniMukherjeee@users.noreply.github.com>
2026-08-06 23:42:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (1.27%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def smallestNumber(self, n, t):
+       
+        while True:
+            p=n
+            pr=1
+            while p>0:
+                pr*=p%10
+                p//=10
+            if pr%t==0:return n
+            n+=1
